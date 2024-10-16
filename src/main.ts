00f83c9fff6b4ba473f0173a0cfb5a67d1467be1
lazaro <lazaroalves12355@gmail.com>
2024-10-16 17:37:28 -0300

    ajueste

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.useStaticAssets(join(__dirname, '..', 'node_modules', 'swagger-ui-dist'));
   app.enableCors({
     origin: "https://adm-gym.vercel.app/",
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
