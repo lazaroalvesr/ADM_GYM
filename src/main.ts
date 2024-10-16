d5a2b68e90b6e79454786fa3adefb59720b43479
lazaro <lazaroalves12355@gmail.com>
2024-10-16 18:53:08 -0300

    ajueste

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'node_modules', 'swagger-ui-dist'));
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
